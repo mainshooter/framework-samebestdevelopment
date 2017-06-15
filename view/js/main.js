671e2d10865eec328517f01c569ad2eeddf3e947
Peter 4A5A <498883@edu.rocmn.nl>
2017-06-15 08:39:50 +0200

    Added documentation

--- view/js/main.js
+++ view/js/main.js
@@ -3,24 +3,40 @@ var ajax;
 
 (function() {
   main = {
-    // The main object for you javascript
+    // The main object for your javascript
   }
 })();
 
 (function() {
   ajax = {
+    /**
+     * Sends a get request with a callback
+     * @param  {[string]} url [Where we want to send the ajax request to]
+     * @return {[obj]}     [The result from ajax request as a object]
+     */
     get_withCallback: function(url) {
       var xhttp = new XMLHttpRequest();
       xhttp.open("GET", url, false);
       xhttp.send();
 
-      return(xhttp.responseText);
+      return(xhttp);
     },
+    /**
+     * Sends a get request without a callback
+     * @param  {[string]} url [The URL where we send the request to]
+     * @return {[object]} xhhtp [The returned object]
+     */
     get_withoutCallBack: function(url) {
       var xhttp = new XMLHttpRequest();
       xhttp.open("GET", url, true);
       xhttp.send();
-    }
+    },
+    /**
+     * Sends a post ajax request with a callback
+     * @param  {[string]} url            [The url where we want to send the post request to]
+     * @param  {[string]} postParameters [With the post values we will fill in]
+     * @return {[type]}                [description]
+     */
     post_withCallback: function(url ,postParameters) {
       var xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
@@ -33,9 +49,14 @@ var ajax;
       xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
       xhttp.send(postParameters);
 
-      return(xhttp.responseText);
+      return(xhttp);
     },
-    post_withoutCallback: function() {
+    /**
+     * [description]
+     * @param  {[string]} url [The url where we want to send the request to]
+     * @param  {[string]} postParameters [With the postParameters in it]
+     */
+    post_withoutCallback: function(url, postParameters) {
       var xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
