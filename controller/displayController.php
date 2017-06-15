1aa492c04b5b9348ab9de8f31280aeeab1cb3da6
Peter 4A5A <498883@edu.rocmn.nl>
2017-06-15 09:59:46 +0200

    Documentation added

--- controller/displayController.php
+++ controller/displayController.php
@@ -1,8 +1,11 @@
 <?php
   require_once 'model/DatabaseHandler.class.php';
-  
+
   class displayController {
 
+    /**
+     * The default method of the controller that present the default view
+     */
     public function default() {
       include 'view/header.php';
         include 'view/result.php';
