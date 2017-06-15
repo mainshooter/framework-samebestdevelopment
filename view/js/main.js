cb09c74bf8f6ebf72a55b6ffca04733fbc9a4569
Peter 4A5A <498883@edu.rocmn.nl>
2017-06-15 07:26:25 +0200

    Written ajax requests

--- view/js/main.js
+++ view/js/main.js
@@ -1,7 +1,51 @@
 var main;
+var ajax;
 
 (function() {
   main = {
     // The main object for you javascript
+  }
+})();
+
+(function() {
+  ajax = {
+    get_withCallback: function(url) {
+      var xhttp = new XMLHttpRequest();
+      xhttp.open("GET", url, false);
+      xhttp.send();
+
+      return(xhttp.responseText);
+    },
+    get_withoutCallBack: function(url) {
+      var xhttp = new XMLHttpRequest();
+      xhttp.open("GET", url, true);
+      xhttp.send();
+    }
+    post_withCallback: function(url ,postParameters) {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          console.log("Done with ajax post with callBack");
+        }
+      };
+      // contains te post values
+      xhttp.open("POST", url, true);
+      xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+      xhttp.send(postParameters);
+
+      return(xhttp.responseText);
+    },
+    post_withoutCallback: function() {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          console.log("Done with ajax post without callBack");
+        }
+      };
+      // contains te post values
+      xhttp.open("POST", url, false);
+      xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+      xhttp.send(postParameters);
+    }
   }
 })();
