5c471c0c618f2bd180c38eafed2aff4c673af108
Peter 4A5A <498883@edu.rocmn.nl>
2017-06-16 07:25:57 +0200

    Documentation added

--- config.php
+++ config.php
@@ -6,6 +6,9 @@
   $config['project-company'] = 'SameBestDevelopment';
 
   $config['base_url'] = '/framework/';
+  // Location of our site;
+  // You need to change this when it is in a diffrent folder our
+  // If it is in the root folder than it must be empty!
   $config['router-debug'] = true;
 
   $config['db-ip'] = '';
@@ -13,6 +16,7 @@
   $config['db-user'] = '';
   $config['db-password'] = '';
   $config['db-name'] = '';
+  // DB config
 
 
   $config['mail-host'] = '';
