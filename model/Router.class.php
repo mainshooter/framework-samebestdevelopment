c2b711426794856f16afe46e60013f0e4f4262bb
Peter 4A5A <498883@edu.rocmn.nl>
2017-06-30 07:32:38 +0200

    New router version

--- model/Router.class.php
+++ model/Router.class.php
@@ -1,81 +1,128 @@
 <?php
-  class Router {
 
-    var $siteLocation;
-    var $defaultController;
-    var $defaultMethod;
+  class Router {
+    public $installedPath = '';
+    public $standardController = '';
+    public $standardMethod = 'index';
+
+    public $translateControllerNames;
+    // $translateControllerNames[name of the translation] = the name of the real controller
+    // Contains the new value for a controller name
 
     private $url;
     private $path;
-
-    private $locationOfThePath;
-    // It is used to track wat we allready have seperated
-
     private $controller;
     private $method;
     private $parameters;
 
-
-    function __construct() {
-
-      $this->url = parse_url($_SERVER['REQUEST_URI']);
-      $this->path = $this->url['path'];
-    }
-
-
-    public function procesTheURL() {
-      if ($this->path == $this->siteLocation) {
-          $this->path = $this->defaultController . '/' . $this->defaultMethod . '/';
-          // The default path what we are gone do
-      }
-      else {
-        $this->path = $this->replaceString($this->siteLocation, '', $this->url['path']);
-        // The slash is te prefent that the router thinks that there are parameters when there arent
-      }
-
-      if (!$this->checkIfLastValueIsASlash($this->path)) {
-        $this->path .= '/';
-      }
-
-
-      $this->controller = $this->getController($this->path) . 'Controller';
-      require_once 'controller/' . $this->controller . ".php";
-
-      $this->method = $this->getMethod($this->path);
-      $this->parameters = $this->getParamters($this->path);
-
-      if ($this->method == '') {
-        // If we don't get a method, we use the default method in the controller
-        $this->method = $this->defaultMethod;
-      }
-
-      $this->sendToDestination();
+    public function __construct() {
+      $this->url = $this->getUrl();
     }
 
     /**
-     * Well we make everything for the call user func array and send it away to the controller
-     * @param  [string] $controller [The name of the controller]
-     * @param  [string] $method     [The name of the method]
-     * @param  [array] $parameters [the parameters we got from the url]
+     * Parse the url to a array
      */
-    private function sendToDestination() {
+    public function parseUrl() {
+        $this->path = str_replace($this->installedPath, '', $this->url);
+        // We remove the installed path from the url
+        // So we get the correct url
+        $this->path = explode('/', $this->path);
+    }
+
+
+    private function translateControllerName() {
+      if (ISSET($this->translateControllerNames[$this->controller])) {
+        // If there is a array in the translate arr with the value we need
+        $this->controller = $this->translateControllerNames[$this->controller];
+      }
+    }
+
+    /**
+     * Parse the router calls the controller, method and send some parameters
+     */
+    public function parseRouter() {
+      require_once 'controller/' . $this->controller . "Controller.php";
+
+      $this->controller = $this->controller . 'Controller';
       $controller = new $this->controller;
       $method = $this->method;
       $parameters = $this->parameters;
-
       call_user_func_array(array($controller, $method), [$parameters]);
-      // Controller is the name of the controller
-      // The method is the method of the controller
-      // parameters is send as a array in a array to be send as a array
     }
 
-    /*
-      Debugs the incoming request
+    /**
+     * Gets the controller from the path
+     * We first check if there is a ctrl
+     * If there isn't a ctrl set we set the default ctrl
      */
-    public function debug() {
+    public function getController() {
+      if (!empty($this->path[0])) {
+        // Check if there is a path
+        $controller = $this->path[0];
+        if (file_exists('controller/' . $controller . 'Controller.php')) {
+          // Check if there is a controller
+          $this->controller = $controller;
+        }
+        else {
+          // We will set the default controller to be used
+          $this->controller = $this->standardController;
+        }
+      }
+
+      else {
+        $this->controller = $this->standardController;
+      }
+    }
+
+    /**
+     * Gets the method
+     * If the method doens't exists we set a default method
+     */
+    public function getMethod() {
+      if (!empty($this->path[1])) {
+        // To check if we have a method comeing in
+        $method = $this->path[1];
+        if (method_exists($this->controller, $method)) {
+          $this->method = $method;
+        }
+
+        else {
+          $this->method = $this->standardMethod;
+        }
+      }
+
+      else {
+        $this->method = $this->standardMethod;
+      }
+    }
+
+    /**
+     * gets the parameters from the path
+     * We first check if there is any parameters
+     */
+    public function getParameters() {
+      if (!empty($this->path[0]) && !empty($this->path[1])) {
+        $parameters = $this->path;
+        unset($parameters[0]);
+        unset($parameters[1]);
+        // To remove the method and the controller
+        $this->parameters = $parameters;
+      }
+      else {
+        $this->parameters = array();
+      }
+
+    }
+
+    public function routerDebug() {
       echo '<div style="font-size: 1.6em;padding: 1em;">';
         echo "<h2 style='padding: 0; margin: 4px;'>Router debug</h2>";
-        echo "Path: " . $this->path;
+        echo "Url: " . $this->url;
+        echo "<br>";
+        echo "Path: ";
+        echo "<pre>";
+          var_dump($this->path);
+        echo "</pre>";
         echo "<br>";
         echo "Controller: " . $this->controller;
         echo "<br>";
@@ -88,145 +135,15 @@
       echo "</div>";
     }
 
-    private function checkIfLastValueIsASlash($string) {
-      $array = $this->stringToArray($string);
-      $arrayLenght = count($array) - 1;
-
-      if ($array[$arrayLenght] != '/') {
-        return(false);
-      }
-      else {
-        return(true);
-      }
-    }
-
     /**
-     * Gets all parameters
-     * @return [array] [With all parameters]
+     * gets the url
+     * @return [string] [The url]
      */
-    private function getParamters() {
-      $location = $this->replaceString($this->locationOfThePath, '', $this->path);
-      // To get remove from the location what we already have done
-
-      $howManyParameters = $this->countSlashes($location);
-
-      for ($i=0; $i < $howManyParameters; $i++) {
-        $this->parameters[] = $this->getNextValue($this->locationOfThePath);
-        // This gets all new parameters
-
-        $this->locationOfThePath .= $this->parameters[$i] . '/';
-        // To save what we already have done
-      }
-
-      return($this->parameters);
+    public function getUrl() {
+      $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+      return($url);
     }
-
-    /**
-     * Get the next value between 2 slashes
-     * @param  [string] $string [The path what we already did]
-     * @return [type]         [description]
-     */
-    private function getNextValue($string) {
-      $string = $this->replaceString($string, '', $this->path);
-      // To get remove from the location what we already have done
-
-      $array = $this->stringToArray($string);
-      $resultArray;
-
-      $pastSlash = 0;
-      foreach ($array as $key) {
-        if ($pastSlash == 0) {
-          if ($key != '/') {
-            $resultArray[] = $key;
-          }
-          else if ($key == '/') {
-            $pastSlash = 1;
-          }
-        }
-      }
-      return($this->arrayToString($resultArray));
-    }
-
-    /**
-     * Gets the method
-     * @return [string] [method name]
-     */
-    private function getMethod() {
-      $this->method = $this->getNextValue($this->locationOfThePath);
-
-      $this->locationOfThePath .= $this->method . '/';
-      // To save what we already have done
-
-      return($this->method);
-    }
-
-    /**
-     * Counts how many / there are for hoe many parameters there are
-     * @param  [string] $string [The path string]
-     * @return [INT]         [How many slashes we have]
-     */
-    private function countSlashes($string) {
-      $array = $this->stringToArray($string);
-
-      $count = 0;
-      foreach ($array as $key) {
-        if ($key == '/') {
-          $count++;
-        }
-      }
-
-      return($count);
-    }
-
-    /**
-     * Gets the name of the controller
-     * @param  [string] $string [The path that we now have]
-     * @return [string] [With the name of the controller]
-     */
-    private function getController($string) {
-      $this->controller = $this->getNextValue($this->locationOfThePath);
-
-      $this->locationOfThePath .= $this->controller . '/';
-      // To save what we already have done
-
-      return($this->controller);
-    }
-
-    /**
-     * Converts a string to a array
-     * @param  [string] $string [The string we want to go to a array]
-     * @return [array] [the result]
-     */
-    private function stringToArray($string) {
-      $array = str_split($string);
-      return($array);
-    }
-
-    /**
-     * Array to string
-     * @param  [1 dept array] $array [The array we want to convert to a string]
-     * @return [string] [The array converted to a string]
-     */
-    private function arrayToString($array) {
-      $string = implode('', $array);
-      return($string);
-    }
-
-    /**
-     * [replaceString description]
-     * @param  [string] $searchString [What we want te replace]
-     * @param  [string] $replace      [What we will replace it with]
-     * @param  [string] $stringSub    [The string that we will use]
-     * @return [string]               [The replaced string]
-     */
-    private function replaceString($searchString, $replace, $stringSub) {
-      $string = str_replace($searchString, $replace, $stringSub);
-      return($string);
-    }
-
-
-
-
-
   }
+
+
 ?>
