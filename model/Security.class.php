803cd7709d1cf77d16c6acfdac2ae141a4787e2c
Peter Romijn <498883@edu.rocmn.nl>
2017-07-22 16:46:05 +0200

    Added a random number generator

--- model/Security.class.php
+++ model/Security.class.php
@@ -15,6 +15,21 @@
       $data = htmlentities($data);
       return ($data);
     }
+
+    /**
+     * Generates a random number
+     * @return [string] [of numbers]
+     */
+    public function generateRandomCode() {
+        $output = rand(1,9);
+        // Prefents the first number to be a 0
+
+        for($i=0; $i<25; $i++) {
+            $output .= rand(0,9);
+        }
+
+        return $output;
+    }
   }
 
 
