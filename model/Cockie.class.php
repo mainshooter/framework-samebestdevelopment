d25727fdea0367c2dbad1d9a1db5926e1d657883
Peter Romijn <498883@edu.rocmn.nl>
2017-07-29 20:46:36 +0200

    Added coockie class with some basic functions

--- model/Cockie.class.php
+++ model/Cockie.class.php
@@ -1,10 +1,33 @@
 <?php
+require_once 'model/Security.class.php';
 
   class cookie {
     var $cookieName;
 
+    /**
+     * Sets the coockieName as a global variable
+     * @param [type] $cookieName [description]
+     */
+    public function __construct($cookieName) {
+      $S = new Security();
+      $coockieName = $S->checkInput($cookieName);
+      $this->cookieName = $coockieName;
+    }
+
+
+    /**
+     * Creates a coockie
+     * @return [boolean] [Returns on succes true on fail a false]
+     */
     public function createCookie() {
-      setcookie($this->cookieName);
+      if (!$this->checkIfCoockieExists()) {
+        setcookie($this->cookieName);
+        return(true);
+      }
+      else {
+        return(false);
+      }
+
     }
 
     /**
@@ -12,15 +35,65 @@
      * @return [string / array] [Returns the value, values or array from a coockie]
      */
     public function readCookie() {
-      return($_COOKIE[$this->cookieName]);
+      if ($this->checkIfCoockieExists()) {
+        return($_COOKIE[$this->cookieName]);
+      }
+
+      else {
+        return(false);
+      }
+
+    }
+
+
+    /**
+     * Updates a coockie
+     * @param  [string] $index [The index name of the arr we want to add]
+     * @param  [type] $value [description]
+     * @return [type]        [description]
+     */
+    public function updateCoockie($index, $value) {
+      if ($this->checkIfCoockieExists()) {
+        $_COOCKIE[$this->coockieName][$index] = $value;
+        return(true);
+      }
+
+      else {
+        return(false);
+      }
+    }
+
+
+    /**
+     * Checks if a coockie exists already
+     * @param  [string] $coockieName [The name of the coockie]
+     * @return [boolean]              [On succes it returns a true]
+     */
+    public function checkIfCoockieExists() {
+      if (ISSET($_COOKIE[$this->cookieName])) {
+        return(true);
+      }
+
+      else {
+        return(false);
+      }
     }
 
 
     /**
      * Delets a coockie
+     * @return [boolean]    [On succes it returns true or false on fails]
      */
     public function deleteCookie() {
-      unset($_COOKIE[$this->cookieName]);
+      if ($this->checkIfCoockieExists()) {
+        unset($_COOKIE[$this->cookieName]);
+        return(true);
+      }
+
+      else {
+        return(false);
+      }
+
     }
   }
 
