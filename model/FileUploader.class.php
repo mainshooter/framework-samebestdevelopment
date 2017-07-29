f784a8a21cb2caa37fe55a72b1bc7e428324f048
Peter Romijn <498883@edu.rocmn.nl>
2017-07-29 22:13:02 +0200

    Basic methods added but now written!

---
+++ model/FileUploader.class.php
@@ -0,0 +1,26 @@
+<?php
+
+  class FileUploader {
+
+    private $allowedFileTypes;
+
+    public function uploadFile() {
+
+    }
+
+    public function checkIfDirectoryExists() {
+
+    }
+
+    public function checkIfFileTypeIsAllowed() {
+
+    }
+
+    public function setAllowedFileTypes() {
+
+    }
+
+  }
+
+
+?>
