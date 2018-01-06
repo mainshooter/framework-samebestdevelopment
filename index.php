dbc34968fe0fdc448982a870a53e05f94e0ac430
Peter 4A5A <498883@edu.rocmn.nl>
2018-01-06 14:54:40 +0100

    Secured my sessions

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
-session_start();
+  session_start();
+  session_regenerate_id(true);
 
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+  ini_set('display_errors', 1);
+  error_reporting(E_ALL);
 
-header('X-Frame-Options: DENY');
+  header('X-Frame-Options: DENY');
 
   require_once 'config.php';
   require_once 'model/Router.class.php';
@@ -12,7 +13,4 @@ header('X-Frame-Options: DENY');
   $Router = new Router($GLOBALS['config']['base_url']);
   $Router->set_default_controller('display');
   $Router->proces_router();
-
-
-
 ?>
